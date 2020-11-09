6158ace473fd321801c695dee4d2909854df0b38
Nikolay Kostyurin <jilizart@gmail.com>
2020-11-09 22:46:12 +0200

    feat: set player scoring

--- src/main.rs
+++ src/main.rs
@@ -46,7 +46,8 @@ fn main() -> amethyst::Result<()> {
         .with_bundle(input_bundle)?
         .with(systems::PaddleSystem, "paddle_system", &["input_system"])
         .with(systems::MoveBallsSystem, "ball_system", &[])
-        .with(systems::BounceSystem, "collision_system", &["paddle_system", "ball_system"]);
+        .with(systems::BounceSystem, "collision_system", &["paddle_system", "ball_system"])
+        .with(systems::WinnerSystem, "winner_system", &["ball_system"]);
 
     let mut game = Application::new(assets_dir, Pong::default(), game_data)?;
 
